45a7bec42dc2336a4dcda9da9dfd612bb3674e22
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-05-01 17:52:52 +0200

    new

--- ebanking-frontend/src/app/accounts/accounts.component.ts
+++ ebanking-frontend/src/app/accounts/accounts.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
   styleUrls: ['./accounts.component.css']
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit{
+  accounts : any;
+  constructor(private http:HttpClient) {}
 
+  ngOnInit(): void {
+    this.http.get("http://localhost:8085/accounts").subscribe(data=>{
+      this.accounts=data;
+    }, error => {
+      console.log(error)
+    })
+  }
 }
--- ebanking-frontend/src/app/customers/customers.component.ts
+++ ebanking-frontend/src/app/customers/customers.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit{
+  customers : any;
   constructor(private http:HttpClient) {}
 
   ngOnInit(): void {
-    
+    this.http.get("http://localhost:8085/customers").subscribe(data=>{
+      this.customers=data;
+    }, error => {
+      console.log(error)
+    })
   }
 }
