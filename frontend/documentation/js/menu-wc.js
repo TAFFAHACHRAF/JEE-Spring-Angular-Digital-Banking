4ea50e312117b060d567e20676827856fb35ccb4
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-05-28 03:58:43 +0200

    generate compodoc documentation

--- frontend/documentation/js/menu-wc.js
+++ frontend/documentation/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">examen documentation</a>
+                    <a href="index.html" data-type="index-link">digital-bancking documentation</a>
                 </li>
 
                 <li class="divider"></li>
